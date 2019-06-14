68e28e97eaa2471acb8a79275aa502d9dba60c9f
Abdelghani Ait Souiri <abdelghani.ait.souiri@gmail.com>
2019-06-13 19:50:23 -0500

    add some changes

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,9 +1,14 @@
 package org.launchcode;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello the world");
+        Scanner in = new Scanner(System.in);
+        System.out.println("whats your name?");
+        String myname = in.nextLine();
+        System.out.println("your name is":+myname);
     }
 }
